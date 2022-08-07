68f55c16cc773145a416942bb5d54751f927a579
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-08-07 19:17:14 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -10,7 +10,3 @@ class User(BaseModel)
        password = ""
        fist_name = ""
        last_name = ""
-  
-def __init__(self, *args, **kwargs):
-        """class constructor"""
-        super().__init__(*args, **kwargs)
