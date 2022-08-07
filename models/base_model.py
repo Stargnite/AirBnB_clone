c909606b703b82f359bf114e6ad33a0fb4f648cd
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-08-07 09:33:40 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,47 @@
-#!/usr/bin/python3 
-"""Difine the BaseModel"""
-import models
+#!/usr/bin/python3
+"""module of 'BaseModel' class"""
+
 from uuid import uuid4
 from datetime import datetime
+import models
 
-   class BaseModel:
-     """ BaseModel of the Airbnb clone Project."""
-     
-    def __int__(self, *args, *kwargs):
-      """BaseModel Construction"""
-      
-      if len(kwargs) == 0:
-         self.id = str(uuid4())
-         self.create_at = datetime.now()
-         self.update_at = self.create_at
-         models.storage.new(self)
-         model.storage(save)
-      else: 
-        format = "%Y-%m-%dt-%H:%M:%S.%f"
-        kwargs["create_at"] = datetime.strptime(kwargs["creat_at"], format)
-        kwargs["update_at"] = datetime.strptime(kwargs["update_at"], format)
-        for key, val in kwargs.item()
-            if "__class__" not in key:
-              setattr(self, key, val)
-              
-      def __str__(self):
-        """
-           print the isinstance
-        """
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-        
-        def self(save):
-          """
-              update the public instance attrivutes
-         """
-         self.update_at = datetime.now()
-         models.storage.save()
-         
-        def to_dict(self):
-          """
-             Returne a Dictinary containing values/ all keys of __dict__ instance
-          """
-        
-        new_dict = dict(self.__dict__)
-        new_dict = ["create_at"] = self.create_at.isoformat(sep='T')
-        new_dict = ["update_ta"] = self.update_at.isoformat(sep='T')
-        new_dict = ["__class__"] = self.class.__name__
-        return new_dict
+
+class BaseModel:
+    """Representation of a BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
+
+    def __str__(self):
+        """custom __str__ method for BaseModel"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+           of __dict__ of the instance"""
+
+        dict_ = dict(self.__dict__)
+        dict_.update({"__class__": self.__class__.__name__,
+                      "created_at": str(((self.created_at).isoformat())),
+                      "updated_at": str(((self.updated_at).isoformat()))})
+        return dict_
