e2a7a6d4d512e013c71682853e4e7dbabf66db2a
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-08-07 19:26:14 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Difining amenity in BaseModel"""
 
-from model.base_models import BaseModel
+from models.base_model import BaseModel
 
    class Amenity(BaseModel)
        """Represente an emenity in BaseModel """
