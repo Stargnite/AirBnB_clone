81b5131880d497ed714848a074c881e6a27eed2b
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-08-07 09:32:34 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,15 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """Define reveiw class from BaseModel"""
 from model.base_models import BaseModel
 
     
     class Review(BaseModel):
-    
-     """Represent a Review.
-     
-       Attrivutes:
-       place_id(str) The usr place id.
-       user_id(str) The usr id.
-       text(str) The text of Review or commet.
-       """
+     """Represent a Review or commet inherits from BaseModel"""
        
     place_id = ""
     user_id = ""
     text = ""
+    
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        super().__init__(*args, **kwargs)
